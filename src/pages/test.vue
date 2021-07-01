b628b5fb1fcb440227a36120ad09d16f8924ee4d
McCarrtney <pmc042333@gmail.com>
2021-07-01 19:16:02 +0800

    change style

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,90 +1,56 @@
 <template>
-  <div>
-    <div class="box">
-      <p>Ant Design Vue</p>
-      <a-steps :current="1">
-        <a-step>
-          <template slot="title">Finished</template>
-          <span slot="description">This is a description.</span>
-        </a-step>
-        <a-step title="In Progress" sub-title="Left 00:00:08" description="This is a description." />
-        <a-step title="Waiting" description="This is a description." />
-      </a-steps>
-    </div>
-
-    <div class="box">
-      <p>Echarts</p>
-      <div class="box1" id="canvas2"></div>
+  <div class="mini-chart">
+    <div class="chart-content" :style="{height: 46}">
+      <v-chart :force-fit="true" :height="height" :data="data" :padding="[36, 5, 18, 5]">
+        <v-tooltip />
+        <v-line position="x*y" />
+        <v-axis></v-axis>
+      </v-chart>
     </div>
   </div>
 </template>
 
 <script>
-import echarts from "echarts";
-require("echarts/lib/chart/bar");
+import {format} from 'date-fns'
+
+const data = []
+const beginDay = new Date().getTime()
+
+const fakeY = [7, 5, 4, 2, 4, 7, 5, 6, 5, 9, 6, 3, 1, 5, 3, 6, 5]
+for (let i = 0; i < fakeY.length; i += 1) {
+  data.push({
+    x: format(new Date(beginDay + 1000 * 60 * 60 * 24 * i), 'yyyy-MM-dd'),
+    y: fakeY[i]
+  })
+}
+
+const tooltip = [
+  'x*y',
+  (x, y) => ({
+    name: x,
+    value: y
+  })
+]
+
+const scale = [{
+  dataKey: 'x',
+  min: 2
+}, {
+  dataKey: 'y',
+  title: '时间',
+  min: 1,
+  max: 22
+}]
+
 export default {
-  data() {
+  name: 'MiniBar',
+  data () {
     return {
-      data: [
-        { year: "1991", value: 3 },
-        { year: "1992", value: 4 },
-        { year: "1993", value: 3.5 },
-        { year: "1994", value: 5 },
-        { year: "1995", value: 4.9 },
-        { year: "1996", value: 6 },
-        { year: "1997", value: 7 },
-        { year: "1998", value: 9 },
-        { year: "1999", value: 13 },
-      ],
-    };
-  },
-
-  props: {},
-
-  components: {},
-
-  created() {},
-
-  computed: {},
-
-  mounted() {
-
-    // Echarts 初始化
-    this.myChart = echarts.init(document.getElementById("canvas2"));
-    this.option = {
-      title: {
-        text: "ECharts 入门示例",
-      },
-      tooltip: {},
-      xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "销量",
-          type: "bar",
-          data: [5, 20, 36, 10, 10, 20],
-        },
-      ],
-    };
-    this.myChart.setOption(this.option);
-  },
-
-  methods: {},
-};
+      data,
+      scale,
+      tooltip,
+      height: 100
+    }
+  }
+}
 </script>
-<style>
-.box {
-  margin: 20px auto;
-  width: 800px;
-  border: 1pxs solid gray;
-  border-radius: 25px;
-  box-shadow: 0 0 10px gray;
-}
-.box1 {
-  margin: 0 auto;
-  width: 800px;
-  height: 500px;
-}
-</style>
--- src/pages/user/info/healthInfo.vue
+++ src/pages/user/info/healthInfo.vue
@@ -1,49 +1,68 @@
 <template>
-  <a-table :columns="columns" :data-source="data" bordered>
-    <template
-      v-for="col in ['date', 'height', 'weight', 'bp', 'lung']"
-      :slot="col"
-      slot-scope="text, record"
-    >
-      <div :key="col">
-        <a-input
-          v-if="record.editable"
-          style="margin: -5px 0"
-          :value="text"
-          @change="e => handleChange(e.target.value, record.key, col)"
-        />
-        <template v-else>
-          {{ text }}
+  <a-card :loading="loading" style="margin-top: 24px" :bordered="false" :body-style="{padding: '24px'}">
+    <a-tabs default-active-key="1">
+      <a-tab-pane key="1" tab="表格显示">
+        <a-table :columns="columns" :data-source="data" bordered>
+          <template
+            v-for="col in ['date', 'height', 'weight', 'bp_high', 'bp_low', 'lung']"
+            :slot="col"
+            slot-scope="text, record"
+          >
+            <div :key="col">
+              <a-input
+                v-if="record.editable"
+                style="margin: -5px 0"
+                :value="text"
+                @change="e => handleChange(e.target.value, record.key, col)"
+              />
+              <template v-else>
+                {{ text }}
+              </template>
+            </div>
+          </template>
+          <template slot="operation" slot-scope="text, record">
+            <div class="editable-row-operations">
+              <span v-if="record.editable">
+                <a @click="() => save(record.key)">Save</a>
+                <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
+                  <a>Cancel</a>
+                </a-popconfirm>
+              </span>
+              <span v-else>
+                <a :disabled="editingKey !== ''" @click="() => edit(record.key)">Edit</a>
+              </span>
+            </div>
+          </template>
+        </a-table>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="图表显示">
+        <template>
+          <div>
+            <v-chart :forceFit="true" :height="height" :data="dateCmputed" :scale="scale">
+              <v-tooltip />
+              <v-axis />
+              <v-line position="date*height" />
+              <v-point position="date*height" shape="circle" />
+            </v-chart>
+          </div>
         </template>
-      </div>
-    </template>
-    <template slot="operation" slot-scope="text, record">
-      <div class="editable-row-operations">
-        <span v-if="record.editable">
-          <a @click="() => save(record.key)">Save</a>
-          <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
-            <a>Cancel</a>
-          </a-popconfirm>
-        </span>
-        <span v-else>
-          <a :disabled="editingKey !== ''" @click="() => edit(record.key)">Edit</a>
-        </span>
-      </div>
-    </template>
-  </a-table>
+      </a-tab-pane>
+    </a-tabs>
+  </a-card>
 </template>
 <script>
+import {format} from 'date-fns'
 const columns = [
   {
     title: '日期',
     dataIndex: 'date',
-    width: '18%',
+    width: '15%',
     scopedSlots: { customRender: 'date' },
   },
   {
     title: '身高',
     dataIndex: 'height',
-    width: '18%',
+    width: '15%',
     scopedSlots: { customRender: 'height' },
     sorter: (a, b) => a.height - b.height,
     sortDirections: ['descend', 'ascend'],
@@ -51,21 +70,31 @@ const columns = [
   {
     title: '体重',
     dataIndex: 'weight',
-    width: '18%',
+    width: '15%',
     scopedSlots: { customRender: 'weight' },
     sorter: (a, b) => a.weight - b.weight,
     sortDirections: ['descend', 'ascend'],
   },
   {
-    title: '血压',
-    dataIndex: 'bp',
-    width: '18%',
-    scopedSlots: { customRender: 'bp' },
+    title: '血压（高压）',
+    dataIndex: 'bp_high',
+    width: '15%',
+    scopedSlots: { customRender: 'bp_high' },
+    sorter: (a, b) => a.bp_high - b.bp_high,
+    sortDirections: ['descend', 'ascend'],
+  },
+  {
+    title: '血压（低压）',
+    dataIndex: 'bp_low',
+    width: '15%',
+    scopedSlots: { customRender: 'bp_low' },
+    sorter: (a, b) => a.bp_low - b.bp_low,
+    sortDirections: ['descend', 'ascend'],
   },
   {
     title: '肺活量',
     dataIndex: 'lung',
-    width: '18%',
+    width: '15%',
     scopedSlots: { customRender: 'lung' },
     sorter: (a, b) => a.lung - b.lung,
     sortDirections: ['descend', 'ascend'],
@@ -76,6 +105,18 @@ const columns = [
     scopedSlots: { customRender: 'operation' },
   },
 ];
+const beginDay = new Date().getTime()
+const begin = format(new Date(beginDay - 1000 * 60 * 60 * 24 * 15), 'yyyy-MM-dd');
+const end = format(new Date(beginDay + 1000 * 60 * 60 * 24 * 15), 'yyyy-MM-dd');
+const scale = [{
+  dataKey: 'date',
+  min: begin,
+  max: end,
+  },{
+  dataKey: 'height',
+  min: 150,
+  max: 250,
+}];
 
 const data = [];
 for (let i = 0; i < 100; i++) {
@@ -83,13 +124,16 @@ for (let i = 0; i < 100; i++) {
     key: i.toString(),
     height: 170+i,
     weight: 70+i,
-    bp: 100+i,
+    bp_high: 120+i,
+    bp_low: 70+i,
     lung: 4000+i,
+    date: format(new Date(beginDay + 1000 * 60 * 60 * 24 * i), 'yyyy-MM-dd')
     // name: `Edrward ${i}`,
     // age: 32,
-    // address: `London Park no. ${i}`,
+    // address: `London Park no. ${i}`
   });
 }
+
 export default {
   data() {
     this.cacheData = data.map(item => ({ ...item }));
@@ -97,7 +141,17 @@ export default {
       data,
       columns,
       editingKey: '',
+      scale,
+      height: 400,
     };
+  },
+  computed: {
+    dateCmputed:function(){
+      const newData = [...this.data];
+      return newData.filter(function(item){
+        return item.date>=begin && item.date<=end
+      })
+    }
   },
   methods: {
     handleChange(value, key, column) {
